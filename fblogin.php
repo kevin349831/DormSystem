b17b3324be79a8027292ec58920061fed673be3c
kevin349831 <kevin349831@gmail.com>
2017-05-12 22:13:43 +0800

    Create fblogin.php

---
+++ fblogin.php
@@ -0,0 +1,48 @@
+<?php
+require 'mysql.php';
+$db = DataBase::initDB();
+session_start();
+$username = z;
+$passwd = z;
+
+//此段為加密過程------
+$key = "146c07ef2479cedcd54c7c2af5cf3a80"; //金鑰
+$salty = "i3flm234rmsldk543kf2jvl2sdfj"; //亂數
+$password_hash = hash_hmac("sha1", $salty + $passwd, $key); //HMAC加料演算法
+//此段為加密過程------
+
+$stmt = $db->query("SELECT * FROM User WHERE User_account='$username'");
+$c = 0;
+foreach ($stmt->fetchAll() as $row) {
+    if ($password_hash == $row['User_passwd']) {
+        $c = 1;
+        $_SESSION['username'] = $username;
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        $datetime = date("Y-m-d H:i:s", mktime(date('H') + 8, date('i'), date('s') + 3, date('m'), date('d'), date('Y')));
+        $user_id=$row['User_ID'];
+        //echo $datetime ; // 顯示時間 
+        $db->exec("INSERT INTO User_Detail(User_Detail_ID, User_IP, User_Login_Date, UserUser_ID) VALUES(NULL, '$ip', '$datetime', '$user_id' )");
+        switch ($row['User_power']) {
+            case '1':
+                header("Location: db2_load.php");
+                break;
+            case '2':
+                header("Location: ./stu/db2_load.php");
+                break;
+        }
+    }
+}
+if ($c == 0) {
+    echo '<script language="javascript">';
+    echo 'alert("帳號或密碼錯誤，請重新輸入")';
+    echo '</script>';
+    header("refresh:0 ; url=loginpage2.html");
+}
+?>
+
