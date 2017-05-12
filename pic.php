63ee610f2ef7c44205e52c438d0e7b6445cae5d9
kevin349831 <kevin349831@gmail.com>
2017-05-12 22:17:56 +0800

    Create pic.php

---
+++ pic.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+    <form action="projet.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
